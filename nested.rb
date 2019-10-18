dda9c2e037313afef1195aeb10d638b316d95fda
William Pannell <lpann5@gmail.com>
2019-10-18 06:16:33 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
+return programmer_hash[:grace_hopper]
 
 
 end
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,9 +54,11 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
+return programmer_hash[:dennis_ritchie][:languages]="C"
+return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -79,8 +83,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+programmer_hash = {:yukihiro_matsumoto => {:known_for => "Ruby",
+          :languages => ["LISP","C"]}}
+return programmer_hash
 end
 
 def changing_alan
@@ -101,7 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for]=alans_new_info
+     return programmer_hash
      
 end
 
@@ -121,5 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]="C","Assembly"
+     return programmer_hash
 
 end
